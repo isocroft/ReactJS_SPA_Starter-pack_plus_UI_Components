cc53a7333884856fbd0ba8b0f1a32c29e5c8fa59
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-08-01 13:36:37 +0000

    accept

--- src/react-busser-components.d.ts
+++ src/react-busser-components.d.ts
@@ -1,9 +1,1 @@
 import React from "react";
-
-//React.HTMLAttributes;
-
-declare module "react" {
-  //   interface HTMLAttributes<HTMLDivElement> extends HTMLAttributes<HTMLElement> {
-  //     align?: "left";
-  //   }
-}
