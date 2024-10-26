10f41f9f90a3dcd465ab5481c1f0dccaf0aa7f1b
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-10-26 15:00:34 +0100

    fix: typo

--- src/components/UI_Widgets/Tabs/external/index.ts
+++ src/components/UI_Widgets/Tabs/external/index.ts
@@ -1,6 +1,6 @@
 import { useSearchParamsState } from "react-busser";
 
-export const useTabs = (activeTabIdQuery= 'active_tab__react-busser', activeTabIndex = 0, replaceInHistory = false) => {
+export const useTabs = (activeTabIdQuery = 'active_tab__react-busser', activeTabIndex = 0, replaceInHistory = false) => {
   const [, setUrlSearchParamsState] = useSearchParamsState(activeTabIdQuery, replaceInHistory, String(activeTabIndex + 1));
   const activate = (currentTabIndex: number) => {
     setUrlSearchParamsState((previousTabIdQuery) => {
