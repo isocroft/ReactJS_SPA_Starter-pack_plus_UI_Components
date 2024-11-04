f1875735664c0cbf9fe5e5588a3f09971755b66f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-11-04 22:03:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
 import "./styles.css";
+import React, { useEffect } from "react";
+import { useBrowserStorage, useBus } from "react-busser";
+import {
+  Link
+} from "react-router-dom";
 
-export default function App() {
+import AppLayout from "./layouts/AppLayout";
+import { ProtectedRoutes, UnProtectedRoutes } from "./routes/routes.config";
+
+export type ExcludeFromProps<P, X extends { [key: string]: unknown }> = Pick<
+  P,
+  Exclude<keyof P, keyof X>
+>;
+
+//export type PickValueType<R> = R[keyof R];
+
+const withAuth = (WrappedComponent: React.FunctionComponent<{ isAuthenticated: boolean }>) => {
+  const WithAuth = (props: ExcludeFromProps<{ isAuthenticated: boolean }, { isAuthenticated: boolean }>) => {
+    const [bus] = useBus({
+      fires: [],
+      subscribes: ["app.logout"]
+    }, "withAuth.LogOut.Routine");
+    const { getFromStorage, clearFromStorage } = useBrowserStorage({ storageType: "session" });
+    const user = getFromStorage<{ email: string, id: string } | null>("__USR_$_", null);
+
+    useEffect(() => {
+      const handleLogOut = () => {
+        clearFromStorage("__USR_$_");
+      };
+
+      bus.on("app.logout", handleLogOut);
+      
+      return () => {
+        bus.off(handleLogOut);
+      }
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    },[]);
+
+    return <WrappedComponent isAuthenticated={user !== null} {...props} />
+  }
+};
+
+function App({ isAuthenticated }) {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <AppLayout className="App" onAppNavigation={({ previousPathname }) => console.log('Navigating from: ', previousPathname) }>
+      <AppLayout.RouteNavigation>
+        {isAuthenticated ? null : (<nav className="">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>)}
+      </AppLayout.RouteNavigation>
+      <AppLayout.RoutePages routes={isAuthenticated ? ProtectedRoutes : UnProtectedRoutes} />
+    </AppLayout>
   );
-}
+};
+
+export default withAuth(App);
