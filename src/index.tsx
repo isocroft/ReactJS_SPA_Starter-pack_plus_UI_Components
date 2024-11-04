8983e32a050757b9bdbf7acf0446acb6b828ee6f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-11-04 22:46:15 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,6 @@ import { envProduction, envDevelopment, envTest } from "./shared/constants";
 import config from "./shared/config";
 import { install } from "resize-observer";
 
-/* @NOTE: `navigator.clipboard` is undefined in Safari 12.1.x as well as the earlier versions 
-  of other browsers like Chrome (Webkit), Firefox, Edge (EdgeHTML) */
-/* @CHECK: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard#clipboard_availability */
-import "clipboard-polyfill/overwrite-globals"; /* @SHIM: # */
-
 if (!window.ResizeObserver) {
   /* @NOTE: This polyfill installation is for the Safari browser only */
   /* @CHECK: https://stackoverflow.com/a/65832955 */
