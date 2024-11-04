dcd6466a4da2a15a7bb128cda462851e81065d9c
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-11-04 22:44:27 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import FeaturesToggleProvider from "./shared/providers/FeaturesToggleProvider";
+import { envProduction, envDevelopment, envTest } from "./shared/constants";
+import config from "./shared/config";
+import { install } from "resize-observer";
+
+/* @NOTE: `navigator.clipboard` is undefined in Safari 12.1.x as well as the earlier versions 
+  of other browsers like Chrome (Webkit), Firefox, Edge (EdgeHTML) */
+/* @CHECK: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard#clipboard_availability */
+import "clipboard-polyfill/overwrite-globals"; /* @SHIM: # */
+
+if (!window.ResizeObserver) {
+  /* @NOTE: This polyfill installation is for the Safari browser only */
+  /* @CHECK: https://stackoverflow.com/a/65832955 */
+  install(); /* @SHIM: # */
+}
+
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <FeaturesToggleProvider enabledFeaturesTable={{
+      [envDevelopment]: config.DEV_FEATURES_LIST,
+      [envTest]: config.DEV_FEATURES_LIST,
+      [envProduction]: config.PROD_FEATURES_LIST
+    }} environment={config.ENV}>
+      <App />
+    </FeaturesToggleProvider>
   </React.StrictMode>
 );
