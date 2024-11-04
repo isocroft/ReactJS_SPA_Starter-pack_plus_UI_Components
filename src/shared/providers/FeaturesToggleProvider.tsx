e1ecf0aa47f6ab69e0c97b7257ec17bbff699fea
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-11-04 22:35:14 +0100

    Create FeaturesToggleProvider.tsx

---
+++ src/shared/providers/FeaturesToggleProvider.tsx
@@ -0,0 +1,30 @@
+import React, { createContext } from "react";
+
+export type FeatureFlagEnvs = "development" | "production" | "test";
+type FeatureEnvsTable = {
+  "development": string[],
+  "test": string[],
+  "production": string[]
+};
+
+const FeaturesToggleContext = createContext({
+  enabledFeatures: [] as string[]
+});
+
+function FeaturesToggleProvider ({
+  children,
+  environment,
+  enabledFeaturesTable
+}: {
+  children: React.ReactNode,
+  environment: FeatureFlagEnvs,
+  enabledFeaturesTable: FeatureEnvsTable
+}) {
+  return (
+    <FeaturesToggleContext.Provider value={{ enabledFeatures: enabledFeaturesTable[environment] }}>
+      {children}
+    </FeaturesToggleContext.Provider>
+  );
+};
+
+export default FeaturesToggleProvider;
