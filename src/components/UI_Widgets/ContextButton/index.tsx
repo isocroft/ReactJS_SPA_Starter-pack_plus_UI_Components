c42355c34685462eab7fab7243a3a3be82525890
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-11-19 20:38:03 +0100

    Create index.tsx

---
+++ src/components/UI_Widgets/ContextButton/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useEffect } from "react";
+import { useFormContext, useFormState } from "react-hook-form";
+
+import Button from "../Button";
+
+import type { ButtonProps } from "../Button";
+
+const ContextSubmitButton: FC<Omit<ButtonProps, "disabled" | "type">> = ({
+  children,
+  className
+  disabled,
+  ...props
+}) => {
+  const { control } = useFormContext();
+  const { isValid, isSubmitting } = useFormState({ control });
+
+  return (
+      <Button
+        {...props}
+        type={"submit"}
+        disabled={!isValid || isSubmitting}
+        className={className}
+      >
+        {children}
+      </Button>
+  );
+};
+
+type ContextSubmitButtonProps = React.ComponentProps<typeof ContextSubmitButton>;
+
+export type { ContextSubmitButtonProps };
+
+export default ContextSubmitButton;
