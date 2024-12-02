2833e0bff2d29e1d377467a18091a5a6b93df2ef
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-12-02 07:35:39 +0100

    Update index.tsx

--- src/components/UI_Widgets/SelectBox/index.tsx
+++ src/components/UI_Widgets/SelectBox/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Ref } from "react";
+import React, { FC, Ref, useEffect } from "react";
 
 import { hasChildren } from "../../../helpers/render-utils";
 
@@ -55,7 +55,7 @@ const SelectBox: FC<
     chevronIconSize?: number;
     chevronIconFillColor?: string;
   } &
-   CustomElementTagProps<"select" | "datalist">
+   CustomElementTagProps<"select">
   > = React.forwardRef(({
   children,
   wrapperClassName,
@@ -65,7 +65,7 @@ const SelectBox: FC<
   chevronIconSize,
   chevronIconFillColor,
   ...props
-}, ref: Ref<HTMLSelectElement | HTMLDataListElement>) => {
+}, ref: Ref<HTMLSelectElement>) => {
   /*
   const onClick = useEventCallback<NonNullable<ComponentProps<"select">["onClick"]>>((event) => {
     if (typeof props.onClick === "function") {
@@ -80,9 +80,61 @@ const SelectBox: FC<
   })
   */
 
+  useEffect(() => {  
+    const styleSheetsOnly = [].slice.call<StyleSheetList, [], StyleSheet[]>(
+      window.document.styleSheets
+    ).filter(
+      (sheet) => {
+        if (sheet.ownerNode) {
+          return sheet.ownerNode.nodeName === "STYLE";
+        }
+        return false;
+    }).map(
+      (sheet) => {
+        if (sheet.ownerNode
+          && sheet.ownerNode instanceof Element) {
+          return sheet.ownerNode.id;
+        }
+        return "";
+    }).filter(
+      (id) => id !== ""
+    );
+
+    if (styleSheetsOnly.length > 0
+      && styleSheetsOnly.includes("react-busser-headless-ui_select")) {
+      return;
+    }
+
+    const selectStyle = window.document.createElement('style');
+    selectStyle.id = "react-busser-headless-ui_select";
+
+    selectStyle.innerHTML = `
+      .select_wrapper-box {
+        position: relative;
+        min-height: auto;
+        font-size: 0;
+      }
+
+      .select_masked {
+        width: 100%;
+        box-sizing: border-box;
+        -webkit-appearance: none;
+        appearance: none;
+        display: block;
+        padding-right: 1.5em;
+      }
+    `;
+
+    window.document.head.appendChild(selectStyle);  
+  
+    return () => {  
+      window.document.head.removeChild(selectStyle);  
+    };  
+  }, []);
+
   return (
-    <div className={wrapperClassname}>
-      <Component {...props} className={className} ref={ref}>{children}</Component>
+    <div className={`select_wrapper-box ${wrapperClassname}`}>
+      <Component {...props} className={`select_masked ${className}`} ref={ref}>{children}</Component>
       {hasChildren(children, 0) ? null : <label htmlFor={id} className={labelClassName}>
           {
             hasChildren(children, 1)
@@ -102,9 +154,9 @@ const SelectBox: FC<
 }
 
 const Option: FC<{
-  ComponentProps<"option">
-}> = ({ value, children }) => {
-  return <option value={value}>{children}</option>
+  React.ComponentProps<"option">
+}> = ({ value, children, ...props }) => {
+  return <option {...props} value={value}>{children}</option>
 }
 
 const SelectBox = Object.assign(SelectBox, { Option });
