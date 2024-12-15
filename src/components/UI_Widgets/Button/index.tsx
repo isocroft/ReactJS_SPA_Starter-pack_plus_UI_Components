39dd254ebca21978b49de5892003fe7791cabe2f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-12-15 15:37:24 +0100

    Update index.tsx

--- src/components/UI_Widgets/Button/index.tsx
+++ src/components/UI_Widgets/Button/index.tsx
@@ -14,6 +14,52 @@ const Button: FC<React.ComponentProps<"button">> = ({
   );
 };
 
+/*
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from "tailwind-merge";
+
+export function clsxn  (...classNameValues: ClassValue[]) {
+  return twMerge(clsx(classNameValues));
+}
+*/
+
+/*
+import React from 'react';
+import Button, { type ButtonProps } from '@/components/UI_Widgets/Button';
+
+import clsxn from './helpers';
+import { cva } from 'class-variance-authority';
+
+const buttonStyles = cva(
+  'base-button',
+  {
+    variants: {
+      size: {
+        small: 'button-small',
+        medium: 'button-medium',
+        large: 'button-large',
+      },
+      color: {
+        primary: 'button-primary',
+        secondary: 'button-secondary',
+      },
+    },
+    defaultVariants: {
+      size: 'medium',
+      color: 'primary',
+    },
+  }
+);
+
+const Button = ({ size, color, children }: ButtonProps) => {
+  return (
+    <Button className={clsxn(buttonStyles({ size, color }))}>
+      {children}
+    </Button>
+  );
+};
+*/
+
 type ButtonProps = React.ComponentProps<typeof Button>;
 
 export type { ButtonProps };
