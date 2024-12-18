bebb7db5fcfdf3a834afa5a7d56d10bf81f83fc7
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-12-18 18:39:12 +0100

    Update index.tsx

--- src/components/UI_Widgets/Table/index.tsx
+++ src/components/UI_Widgets/Table/index.tsx
@@ -1,2 +1,21 @@
-import React from "react";
-import { useTable } from "react-table";
+import React, { FC } from "react";
+//import { useTable } from "react-table";
+
+type CustomCellRendererProps<D = {}, V> = {
+  cellValue: D[keyof D];
+  cellValueAsFormatted: V;
+  rowData: D;
+};
+
+type ColumnOptions<T = {}, R extends string | number | unknown> = {
+  headerRender: () => ;
+  field: keyof T;
+  valueFormatter: (value: T[keyof T]) => R;
+  cellRenderer: (cellDetails: CustomCellRendererProps<T, R>) => React.ReactNode;
+};
+
+const Table: FC<> = ({ columns, data,  ...props }) => {
+  return <table {...props}>
+  
+  </table>
+};
