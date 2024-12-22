d81134c9d1578c7f2e91c918e5f7be9e721033a9
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2024-12-22 06:47:54 +0100

    Update config.ts

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,12 +2,12 @@ export default {
   PROD: process.env.NODE_ENV === "production",
   ENV: process.env.NODE_ENV,
   DEV_FEATURES_LIST: [
-    /* @HINT: all features shown to a user on {https://localhost:3100} LOCAL deploy (development env) except the ones with an underscore at the end */
-    "page:home|ui=[nav,footer]",
-    "page:vehicles|ui=[list];role=[admin];percentage=0.25"
+    /* @HINT: all features shown to a user on {https://localhost:3100} LOCAL deploy (development env) except the ones with an underscore at the begining */
+    "page.menuitem:home|segments=[*]",
+    "page.menuitem:vehicles|role=[admin];qualifier=0.25"
   ],
   PROD_FEATURES_LIST: [
-    /* @HINT: all features shown to a user on {https://myapp.com} LOCAL deploy (production env) except the ones with an underscore at the end */
-    "page:home_"
+    /* @HINT: all features shown to a user on {https://myapp.com} LOCAL deploy (production env) except the ones with an underscore at the begining */
+    "_page.menuitem:home"
   ]
 };
