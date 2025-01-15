ae50ea6fc439d6dae59c403300260108d96c7507
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-15 23:30:18 +0100

    Update web_page-utils.ts

--- src/helpers/web_page-utils.ts
+++ src/helpers/web_page-utils.ts
@@ -1,11 +1,119 @@
+/**
+ * verifyDOMElementIsWithinViewPort:
+ *
+ * @param {HTMLElement} element
+ *
+ * @returns {Boolean}
+ */
+export const verifyDOMElementIsWhollyWithinViewport = (element: HTMLElement) => {
+  const rect = element.getBoundingClientRect();
+
+  const minimumYFrame = 0;
+  const minimumXFrame = 0;
+  const maximumYFrame = (window.innerHeight || document.documentElement.clientHeight);
+  const maximumXFrame = (window.innerWidth || document.documentElement.clientWidth);
+
+  return (
+    rect.top >= minimumYFrame &&  
+    rect.left >= minimumXFrame &&  
+    rect.bottom <= maximumYFrame &&  
+    rect.right <= maximumXFrame
+  );
+};
 
 /*!
- * @EXAMPLE: 
+ * @EXAMPLE:
+ * 
+ * const isElementVisibleInViewport = verifyDOMElementIsWhollyWithinViewport(
+ *   document.querySelector('[id="compactor"]')
+ * )
  *
- * const decodedHTML = htmlDecode("&lt;h1&gt;Hi there!&lt;/h1&gt;");
+ * console.log(sElementVisibleInViewport) // true
+ */
+
+/**
+ * verifyDOMElementIsNotWithinViewPort:
  *
- * console.log(decodedHTML); // "<h1>Hi there!</h1>"
+ * @param {HTMLElement} element
  *
+ * @returns {Boolean}
+ */
+export const verifyDOMElementIsNotWithinViewport = (element: HTMLElement) => {
+  const rect = element.getBoundingClientRect();
+
+  const minimumYFrame = 0;
+  const minimumXFrame = 0;
+  const maximumYFrame = (window.innerHeight || document.documentElement.clientHeight);
+  const maximumXFrame = (window.innerWidth || document.documentElement.clientWidth);
+	
+  return (
+    (rect.top < minimumYFrame &&
+      rect.bottom < minimumYFrame) ||
+    (rect.left < minimumXFrame &&
+      rect.right < minimumXFrame) ||
+    rect.y > maximumYFrame ||
+    rect.x > maximumXFrame
+  );
+};
+
+/*!
+ * @EXAMPLE:
+ * 
+ * const isElementNotVisibleInViewport = verifyDOMElementIsNotWithinViewport(
+ *   document.querySelector('[id="compactor"]')
+ * )
+ *
+ * console.log(isElementNotVisibleInViewport) // false
+ */
+
+/**
+ * verifyDOMElementIsOnlyPartiallyWithinViewPort:
+ *
+ * @param {HTMLElement} element
+ *
+ * @returns {Boolean}
+ */
+export const verifyDOMElementIsOnlyPartiallyWithinViewPort = (element: HTMLElement) => {  
+  return !(
+    verifyDOMElementIsWhollyWithinViewport(element)
+  ) && !(
+    verifyDOMElementIsNotWithinViewport(element)
+  );  
+};
+
+/*!
+ * @EXAMPLE:
+ * 
+ * const isElementPartialyVisibleInViewport = verifyDOMElementIsOnlyPartiallyWithinViewPort(
+ *   document.querySelector('[id="compactor"]')
+ * )
+ *
+ * console.log(isElementPartialyVisibleInViewport) // true
+ */
+
+/**
+ * verifyDOMElementIsWhollyOrPartiallyWithinViewPort:
+ *
+ * @param {HTMLElement} element
+ *
+ * @returns {Boolean}
+ */
+export const verifyDOMElementIsWhollyOrPartiallyWithinViewPort = (element: HTMLElement) => {  
+  return (
+    verifyDOMElementIsWhollyWithinViewport(element)
+  ) && !(
+    verifyDOMElementIsNotWithinViewport(element)
+  );  
+};
+
+/*!
+ * @EXAMPLE:
+ * 
+ * const isElementPartialyOrWhollyVisibleInViewport = verifyDOMElementIsWhollyOrPartiallyWithinViewPort(
+ *   document.querySelector('[id="compactor"]')
+ * )
+ *
+ * console.log(isElementPartialyOrWhollyVisibleInViewport) // true
  */
 
 /**
