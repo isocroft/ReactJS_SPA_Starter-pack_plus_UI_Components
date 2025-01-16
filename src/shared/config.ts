99e879db14a5c8d0d984b0ed81bf9d96a51301e5
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-16 01:01:16 +0100

    Update config.ts

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -4,10 +4,10 @@ export default {
   DEV_FEATURES_LIST: [
     /* @HINT: all features shown to a user on {https://localhost:3100} LOCAL deploy (development env) except the ones with an underscore at the begining */
     "page:home|segments=[*]",
-    "page.menuitem:vehicles|role=[admin];qualifier=0.25"
+    "page.menuitem:vehicles|role=[admin];qualifier=0.25" /* 'page.menuitem...' is equivalent to 'page...|segments=[menuitem]'
   ],
   PROD_FEATURES_LIST: [
     /* @HINT: all features shown to a user on {https://myapp.com} LOCAL deploy (production env) except the ones with an underscore at the begining */
-    "_page.menuitem:home"
+    "_page:home"
   ]
 };
