7128848da8a41444265a6467059b5019e1084fc8
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-16 20:05:03 +0100

    Update config.ts

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,8 @@
 export default {
   PROD: process.env.NODE_ENV === "production",
+  BUGSNAG: {
+    apiKey: process.env.BUGSNAG_API_KEY
+  },
   ENV: process.env.NODE_ENV,
   DEV_FEATURES_LIST: [
     /* @HINT: all features shown to a user on {https://localhost:3100} LOCAL deploy (development env) except the ones with an underscore at the begining */
