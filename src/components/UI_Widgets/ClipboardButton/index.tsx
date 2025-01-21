eef5a1fa5d600a84ce9fb129ea682d480b64609a
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-21 16:19:46 +0100

    Update index.tsx

--- src/components/UI_Widgets/ClipboardButton/index.tsx
+++ src/components/UI_Widgets/ClipboardButton/index.tsx
@@ -11,21 +11,23 @@ export function ClipboardButton({
   children,
   textToCopy,
   ...props
-}: Omit<ButtonProps, "onClick" | "type"> & {
+}: Omit<ButtonProps, "onClick" | "type" | "disabled"> & {
   textToCopy: string;
 }) {
   const commands = useUICommands();
+  const canCopy = typeof textToCopy === "string" && textToCopy.length > 0;
   return (
     <Button
       type="button"
       data-clipboard-trigger="active"
       onClick={() => {
-        if (typeof textToCopy === "string" && textToCopy.length > 0) {
+        if (canCopy) {
           commands.hub.copy(COPY_COMMAND, textToCopy);
         }
       }}
       {...props}
       className={className}
+      disabled={!canCopy}
     >
       {children}
     </Button>
