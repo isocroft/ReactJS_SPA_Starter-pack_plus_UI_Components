1cefed1092f2d5bcd4874071c1655164c823a339
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-21 15:51:40 +0100

    Update index.tsx

--- src/components/UI_Widgets/InputBox/index.tsx
+++ src/components/UI_Widgets/InputBox/index.tsx
@@ -1,7 +1,22 @@
 import React, { FC, Ref, useEffect, useRef } from "react";
 
 const InputBox: FC<React.ComponentProps<"input">> = React.forwardRef(({
+  id,
+  name,
+  type = "text",
+  src,
+  size,
+  onChange,
+  children,
+  className,
+  defaultValue = "",
+  valueSync = false,
+  tabIndex = 0,
+  ...props
+}, ref: Ref<HTMLInputElement>) => {
+  const anyValue = (
+    defaultValue !== "" ? defaultValue : props.value
+  ) as string;
+  const textBoxRef = useRef<(HTMLInputElement & HTMLTextAreaElement) | null>(null);
   
-}) => {
-
 });
