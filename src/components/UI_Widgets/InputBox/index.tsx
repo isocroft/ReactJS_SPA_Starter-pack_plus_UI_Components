f865aace7f1e095ccbf7f14cb678f07d2990cd6f
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-21 15:55:03 +0100

    Update index.tsx

--- src/components/UI_Widgets/InputBox/index.tsx
+++ src/components/UI_Widgets/InputBox/index.tsx
@@ -1,22 +1,45 @@
 import React, { FC, Ref, useEffect, useRef } from "react";
 
 const InputBox: FC<React.ComponentProps<"input">> = React.forwardRef(({
-  id,
-  name,
   type = "text",
-  src,
-  size,
-  onChange,
-  children,
-  className,
   defaultValue = "",
   valueSync = false,
-  tabIndex = 0,
   ...props
 }, ref: Ref<HTMLInputElement>) => {
   const anyValue = (
     defaultValue !== "" ? defaultValue : props.value
   ) as string;
-  const textBoxRef = useRef<(HTMLInputElement & HTMLTextAreaElement) | null>(null);
-  
+  const inputBoxRef = useRef<(HTMLInputElement & HTMLTextAreaElement) | null>(null);
+  useEffect(() => {
+    if (!valueSync || inputBoxRef.current === null || anyValue === "") {
+      return;
+    }
+    /* @NOTE: Programmatically trigger a `change` event on a <input> tag */
+    /* @CHECK: https://github.com/facebook/react/issues/19678#issuecomment-679044981 */
+    const programmaticChangeEvent = new Event("input", { bubbles: true });
+    const setInputValue = Component === "textarea"
+      ? Object.getOwnPropertyDescriptor(
+        HTMLTextAreaElement.prototype,
+        "value"
+      )!.set
+      : Object.getOwnPropertyDescriptor(
+      HTMLInputElement.prototype,
+      "value"
+    )!.set;
+
+    if (typeof setInputValue !== "undefined") {
+      setInputValue.call(
+        inputBoxRef.current,
+        anyValue
+      );
+      inputBoxRef.current.dispatchEvent(programmaticChangeEvent);
+    }
+  /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [valueSync, anyValue]);
+
+  return (
+    <input
+      {...props}
+    />
+  );
 });
