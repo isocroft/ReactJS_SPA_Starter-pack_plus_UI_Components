d8d4326f68d689c0c482081792e444d1260a69cf
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-22 22:27:30 +0100

    Update index.tsx

--- src/components/UI_Widgets/Fieldset/index.tsx
+++ src/components/UI_Widgets/Fieldset/index.tsx
@@ -1,1 +1,58 @@
 import React  from "react";
+
+/*
+
+window.open('tel:+2349038829558', '_blank');
+
+.text-input-wrapper .input-wrapper {
+  outline: 1px solid #e1e1e1;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  border-radius: 5px;
+  position: relative;
+  transition: background-color .2s cubic-bezier(0,0,.2,1) 0ms;
+  width: 100%;
+  cursor: text;
+  box-sizing: border-box;
+  align-items: center;
+  letter-spacing: .00938em;
+}
+
+.text-input-wrapper .input-wrapper:focus-within:after, /* Newer Browsers * /
+.text-input-wrapper input:focus + .input-wrapper:after { /* Older Browsers * /
+  content: "\a0";
+  box-shadow: inset 0 -2px 0 var(--custom-focus-color);
+  border-bottom: none;
+  transform: scaleX(1);
+}
+
+.text-input-wrapper .input-wrapper:after {
+  left: 0;
+  right: 0;
+  bottom: 0;
+  content: "";
+  position: absolute;
+  transform: scaleX(0);
+  transition: transform .2s cubic-bezier(0,0,.2,1) 0ms;
+  border-bottom: 2px solid var(--custom-focus-color);
+  pointer-events: none;
+}
+
+<fieldset class="text-input-wrapper">
+  <div class="input-wrapper">
+    <label for="input">Text</label>
+    <input name="input" placeholder="Enter text..." id="input" type="text" />
+  </div>
+</fieldset>
+
+<button id="button">
+  <object data="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/check.svg" type="image/svg+xml" style="height:40px;width:40px;">
+    Mark
+  </object>
+</button>
+
+*/
+
+const Fieldset = ({ children, ...props }: React.ComponentProps<"fieldset">) => {
+  
+};
