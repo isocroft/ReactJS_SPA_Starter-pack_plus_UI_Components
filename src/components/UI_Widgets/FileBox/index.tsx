e40e00ee45a752ef84d7777b4f148d1e0f407860
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-22 22:55:28 +0100

    Update index.tsx

--- src/components/UI_Widgets/FileBox/index.tsx
+++ src/components/UI_Widgets/FileBox/index.tsx
@@ -1,1 +1,57 @@
-import React from "react";
+import React, { FC, useRef, useEffect } from "react";
+
+import { hasChildren } from "../../../helpers/render-utils";
+
+const FileBox: FC<Pick<React.ComponentProps<"input">, "accept" | "webkitdirectory" | "multiple"> & {
+  wrapperClassname?: string;
+  labelPosition?: "beforeInput" | "afterInput"
+}> = ({
+  id,
+  name,
+  tabIndex = 0,
+  labelPosition = "beforeInput",
+  accept,
+  webkitdirectory = false,
+  multiple = false,
+  children,
+  ...props
+}) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (fileInputRef.current !== null) {
+      /* @NOTE: ReactJS does not yet support the `onCancel` event on file inputs */
+      /* @CHECK: https://github.com/facebook/react/issues/27858 */
+      fileInputRef.adddEventListener('cancel', () => {
+        // @TODO: ...
+      });
+    }
+
+    
+  }, []);
+
+  return (
+    <div id={id} name={name} tabIndex={tabIndex} {...props}>
+      {hasChildren(children, 0) ? null : (labelPosition === "beforeInput" && (<label htmlFor={id}>
+        {children}
+      </label>) || null)}
+      <input
+        id={id}
+        name={name}
+        type={"file"}
+        accept={accept}
+        webkitdirectory={webkitdirectory ? "" : undefined}
+        multiple={multiple ? "" : undefined}
+      />
+      {hasChildren(children, 0) ? null : (labelPosition === "afterInput" && (<label htmlFor={id}>
+        {children}
+      </label>) || null)}
+    </div>
+  );
+};
+
+type FileBoxProps = React.ComponentProps<typeof FileBox>;
+
+export type { FileBoxProps };
+
+export default FileBox;
