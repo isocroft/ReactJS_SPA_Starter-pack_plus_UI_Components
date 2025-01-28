19a9299b874473eb5d723aceb82ca070c63f0306
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-28 12:45:28 +0100

    Update index.tsx

--- src/components/UI_Widgets/InputBox/index.tsx
+++ src/components/UI_Widgets/InputBox/index.tsx
@@ -1,57 +1,59 @@
-import React, { FC, Ref, useEffect, useRef } from "react";
+import React, { Ref, useRef, useCallback, useEffect } from "react";
 
-const InputBox: FC<React.ComponentProps<"input">> = React.forwardRef(({
-  type = "text",
-  defaultValue = "",
-  tabIndex = 0,
-  valueSync = false,
-  ...props
-}, ref: Ref<HTMLInputElement>) => {
-  const anyValue = (
-    defaultValue !== "" ? defaultValue : props.value
-  ) as string;
-  const inputBoxRef = useRef<HTMLInputElement | null>(null);
-  useEffect(() => {
-    if (!valueSync || inputBoxRef.current === null || anyValue === "") {
-      return;
-    }
-    /* @NOTE: Programmatically trigger a `change` event on a <input> tag */
-    /* @CHECK: https://github.com/facebook/react/issues/19678#issuecomment-679044981 */
-    const programmaticChangeEvent = new Event("input", { bubbles: true });
-    const setInputValue = Object.getOwnPropertyDescriptor(
-      HTMLInputElement.prototype,
-      "value"
-    )!.set;
-
-    if (typeof setInputValue !== "undefined") {
-      setInputValue.call(
-        inputBoxRef.current,
-        anyValue
-      );
-      inputBoxRef.current.dispatchEvent(programmaticChangeEvent);
-    }
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [valueSync, anyValue]);
-
-  return (
-    <input
-      type={type}
-      tabIndex={tabIndex}
-      {...props}
-      defaultValue={
-        !props.value && defaultValue !== "" ? defaultValue : undefined
+const InputBox = React.forwardRef(
+  (
+    {
+      type = "text",
+      defaultValue = "",
+      tabIndex = 0,
+      valueSync = false,
+      ...props
+    }: React.ComponentProps<"input"> & { valueSync: boolean },
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const anyValue = (defaultValue ? defaultValue : props.value) as string;
+    const inputBoxRef = useRef<HTMLInputElement | null>(null);
+    useEffect(() => {
+      if (!valueSync || inputBoxRef.current === null || anyValue === "") {
+        return;
       }
-      ref={(node?: HTMLInputElement) => {
-        if (node) {
-          inputBoxRef.current = node;
-        } else {
-          inputBoxRef.current = null;
+      /* @NOTE: Programmatically trigger a `change` event on a <input> tag */
+      /* @CHECK: https://github.com/facebook/react/issues/19678#issuecomment-679044981 */
+      const programmaticChangeEvent = new Event("input", { bubbles: true });
+      const setInputValue = Object.getOwnPropertyDescriptor(
+        HTMLInputElement.prototype,
+        "value"
+      )!.set;
+
+      if (typeof setInputValue !== "undefined") {
+        setInputValue.call(inputBoxRef.current, anyValue);
+        inputBoxRef.current.dispatchEvent(programmaticChangeEvent);
+      }
+      /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [valueSync, anyValue]);
+
+    const $ref = useCallback((node) => {
+      if (node) {
+        inputBoxRef.current = node;
+      } else {
+        inputBoxRef.current = null;
+      }
+      return typeof ref === "function" ? ref(node) : ref;
+    }, []);
+
+    return (
+      <input
+        type={type}
+        tabIndex={tabIndex}
+        {...props}
+        defaultValue={
+          !props.value && defaultValue !== "" ? defaultValue : undefined
         }
-        return typeof ref === "function" ? ref(node) : ref;
-      }}
-    />
-  );
-});
+        ref={$ref}
+      />
+    );
+  }
+);
 
 type InputBoxProps = React.ComponentProps<typeof InputBox>;
 
