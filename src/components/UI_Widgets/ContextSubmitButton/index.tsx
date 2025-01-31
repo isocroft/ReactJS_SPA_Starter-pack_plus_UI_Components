176b6e045e8d6dd3bb5787c63566bb06d42b1403
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-01-31 12:38:48 +0100

    Update index.tsx

--- src/components/UI_Widgets/ContextSubmitButton/index.tsx
+++ src/components/UI_Widgets/ContextSubmitButton/index.tsx
@@ -1,33 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
 import { useFormContext, useFormState } from "react-hook-form";
 
 import Button from "../Button";
 
 import type { ButtonProps } from "../Button";
 
-const ContextSubmitButton: Omit<ButtonProps, "disabled" | "type"> & { isLoading: boolean } = ({
-  children,
-  className,
-  isLoading = false,
-  disabled,
-  ...props
-}) => {
+const ContextSubmitButton: FC<
+  Omit<ButtonProps, "disabled" | "type"> & { isLoading: boolean }
+> = ({ children, className = "", isLoading = false, ...props }) => {
   const { control } = useFormContext();
   const { isValid, isSubmitting } = useFormState({ control });
 
   return (
-      <Button
-        {...props}
-        type={"submit"}
-        disabled={!isValid || isSubmitting || isLoading}
-        className={className}
-      >
-        {children}
-      </Button>
+    <Button
+      {...props}
+      type={"submit"}
+      disabled={!isValid || isSubmitting || isLoading}
+      className={className}
+    >
+      {children}
+    </Button>
   );
 };
 
-type ContextSubmitButtonProps = React.ComponentProps<typeof ContextSubmitButton>;
+type ContextSubmitButtonProps = React.ComponentProps<
+  typeof ContextSubmitButton
+>;
 
 export type { ContextSubmitButtonProps };
 
