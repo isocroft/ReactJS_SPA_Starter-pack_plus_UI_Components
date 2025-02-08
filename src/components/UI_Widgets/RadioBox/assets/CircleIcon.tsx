08e15687da72c3c13128b06c9d7ea69984c989c0
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-08 13:32:35 +0100

    Update CircleIcon.tsx

--- src/components/UI_Widgets/RadioBox/assets/CircleIcon.tsx
+++ src/components/UI_Widgets/RadioBox/assets/CircleIcon.tsx
@@ -8,7 +8,7 @@ interface CircleIconProps extends SVGAttributes<SVGElement> {
 
 export const CircleIcon = ({ size = 16, iconFill = "#ffffff", iconStroke = "currentColor", ...props }: CircleIconProps) => {
   return (
-    <svg  width={size} height={size} viewBox="0 0 26 26" fill="none" {...props}>
+    <svg  width={size} height={size} viewBox="0 0 26 26" fill="none" {...props} role="presentation">
       <rect x={0.5} y={0.5} width={size + 1} height={size + 1} rx={Number((size + 1/2).toFixed(1))} fill={iconFill} stroke={iconStroke} />
     </svg>
   );
