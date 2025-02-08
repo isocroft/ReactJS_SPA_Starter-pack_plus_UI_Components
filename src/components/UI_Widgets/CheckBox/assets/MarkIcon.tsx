b3b1bf83876f2985c8eff1f362f3fb41383f32f8
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-08 13:33:04 +0100

    Update MarkIcon.tsx

--- src/components/UI_Widgets/CheckBox/assets/MarkIcon.tsx
+++ src/components/UI_Widgets/CheckBox/assets/MarkIcon.tsx
@@ -8,7 +8,7 @@ interface MarkIconProps extends SVGAttributes<SVGElement> {
 
 export const MarkIcon = ({ size = 16, iconFill = "#0fE1ab", iconStroke = "currentColor", ...props }: MarkIconProps) => {
   return (
-    <svg  width={size} height={size} viewBox="0 0 26 26" fill="none" {...props}>
+    <svg  width={size} height={size} viewBox="0 0 26 26" fill="none" {...props} role="presentation">
       <rect width={size} height={size} rx={Number((size/2).toFixed(1))} fill={iconFill} />
       <path
         d="M8.6665 12.6L10.7617 15L12.0713 13.5M15.9998 9L13.3808 12"
