c11ec20e1cb5200ad5e93adb60cef2e99db1c026
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-15 04:53:39 +0100

    Update index.tsx

--- src/components/UI_Widgets/Avatar/index.tsx
+++ src/components/UI_Widgets/Avatar/index.tsx
@@ -1,11 +1,12 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import defaultImagePath from "./assets/svg/photo-holder.svg";
 
 export enum AvatarSizes {
   Tiny="tn",
-  Small= "sm",
-  Medium= "md",
-  Large= "lg"
+  Small="sm",
+  Medium="md",
+  Large="lg",
+  XtraLarge="xlg"
 }
 
 export interface AvatarProps extends React.ComponentPropsWithRef<"div"> {
@@ -13,19 +14,23 @@ export interface AvatarProps extends React.ComponentPropsWithRef<"div"> {
   imageClassName?: string;
   letterClassName?: string;
   src?: string;
+  widgetSize?: number;
   letters?: string;
+  alt?: string;
 }
   
 const Avatar: FC<AvatarProps> = ({
-  size = AvatarSizes.Small,
+  size,
   src = defaultImagePath,
-  className,
-  imageClassName,
-  letterClassName,
+  className = "",
+  imageClassName = "",
+  letterClassName = "",
+  alt = "avatar",
+  widgetSize = 40,
   letters,
   ...props
 }: AvatarProps) => {
-  React.useEffect(() => {  
+  useEffect(() => {  
     const styleSheetsOnly = [].slice.call<StyleSheetList, [], StyleSheet[]>(
       window.document.styleSheets
     ).filter(
@@ -46,6 +51,7 @@ const Avatar: FC<AvatarProps> = ({
     );
 
     if (styleSheetsOnly.length > 0
+      /* @ts-ignore */
       && styleSheetsOnly.includes("react-busser-headless-ui_avatar")) {
       return;
     }
@@ -55,40 +61,82 @@ const Avatar: FC<AvatarProps> = ({
 
     avatarStyle.innerHTML = `  
       .avatar_wrapper-box {
-        display: inline-flex;
         display: inline-block;
         overflow: hidden;
+        border-radius: 50%;
       }
 
       .avatar_letter-box {
         display: inline-block;
         vertical-align: middle;
         text-transform: uppercase;
-        text-align: center;
         position: relative;
       }
-    
-      .avatar_image-box {
-        object-fit: cover;
+
+      .avatar_letter-box svg {
+        opacity: 0;
+        margin: 0;
+        padding: 0;
+        display: block;
+      }
+
+      .avatar_letter-box[data-letter-size] svg {
+        display: none;
+        visibility: hidden;
+      }
+
+      .avatar_letter-box[data-letter]::after {
+        display: inline-flex;
+        position: absolute;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        left: 0;
+        top: 0;
         width: 100%;
+        height: 100%;
+        content: attr(data-letter);
+      }
+    
+      .avatar_image-box[width] {
+        object-fit: cover;
         display: inline-block;
+        aspect-ratio: 1 / 1;
         height: auto;
       }
 
+      img[data-responsize="true"] {
+        width: 100%;
+      }
+
       .avatar_letter-box[data-letter-size="tn"],
-      .avatar_image-box[data-letter-size="tn"] {
+      .avatar_image-box[data-image-size="tn"] {
         width: 24px;
         height: 24px;
       }
 
-      .avatar_letter-box[data-letter-size="sm"] {
+      .avatar_letter-box[data-letter-size="sm"],
+      .avatar_image-box[data-image-size="sm"] {
         width: 40px;
         height: 40px;
       }
 
-      .avatar_letter-box[data-letter-size="md"] {
+      .avatar_letter-box[data-letter-size="md"],
+      .avatar_image-box[data-image-size="md"] {
         width: 60px;
         height: 60px;
+      }
+
+      .avatar_letter-box[data-letter-size="lg"],
+      .avatar_image-box[data-image-size="lg"] {
+        width: 80px;
+        height: 80px;
+      }
+
+      .avatar_letter-box[data-letter-size="xlg"],
+      .avatar_image-box[data-image-size="xlg"] {
+        width: 100px;
+        height: 100px;
       }
     `;  
     window.document.head.appendChild(avatarStyle);  
@@ -98,12 +146,63 @@ const Avatar: FC<AvatarProps> = ({
     };  
   }, []);
 
+  const sizeMap = {
+    "xlg": 100,
+    "lg": 80, 
+    "md": 60,
+    "sm": 40,
+    "tn": 24
+  } as const;
+
   return (
-    <div className={`${className} avatar_wrapper-box`} data-letter={letters ? letters : undefined} {...props}>
+    <div className={`${className} avatar_wrapper-box`} {...props}>
       {letters
-        ? <span className={`${letterClassName} avatar_letter-box`} data-letter-size={letters ? size : undefined}>{letters}</span>
-        : <img alt="avatar" className={`${imageClassName} avatar_image-box`} src={src} data-image-size={size ? size : undefined} />
+        ? <span
+            className={`${letterClassName} avatar_letter-box`}
+            data-letter-size={size ? size : undefined}
+            data-letter={letters}
+            title={alt}
+          >
+          <svg
+            width={widgetSize}
+            height={widgetSize}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            role="generic"
+          >  
+            <rect width={widgetSize} height={widgetSize} />  
+          </svg>
+        </span>
+        : <img
+            alt={alt}
+            title={alt}
+            className={`${imageClassName} avatar_image-box`}
+            src={src}
+            data-image-size={size ? size : undefined}
+            width={size ? sizeMap[size] : widgetSize}
+            height={size ? sizeMap[size] : widgetSize}
+          />
       }
     </div>
   );
 };
+
+export const AvatarWidget = {
+  WidgetSizes: {
+    TINY: 24,
+    SMALL: 40,
+    MID: 60,
+    LARGE: 80,
+    XTRALARGE: 100
+  }
+};
+
+/*
+  <Avatar
+    widgetSize={48}
+    letters={"MN"}
+    alt={"Minas Nerith"}
+  />
+*/
+
+export default Avatar;
