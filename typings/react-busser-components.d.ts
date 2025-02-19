d8e3a68c3128915f088ee8ee65b37307af13e404
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-19 04:05:17 +0100

    Update react-busser-components.d.ts

--- typings/react-busser-components.d.ts
+++ typings/react-busser-components.d.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import { Span } from "@opentelemetry/api";
 
 export {};
 
@@ -6,13 +7,66 @@ declare class Stringified<T> extends String {
   private ___stringified: T
 }
 
+type HeadwayConfig = {
+  selector: string;
+  account: string;
+  trigger: string;
+  position?: {
+    x: string;
+    y: string
+  };
+  translations?: {
+    title: string;
+    readMore: string;
+    labels: Record<string, string>;
+    footer: string;
+  };
+  callbacks?: {
+    onWidgetReady: (widget?: { getUnseenCount: Function }) => void;
+    onShowWidget: () => void;
+    onShowDetails: (changelog: { id: string; title: string }) => void;
+    onHideWidget: () => void;
+  };
+};
+
+type NewRelicInteraction = {
+  save: () => void;
+  setName: (name: string, trigger?: string) => NewRelicInteraction;
+  setAttribute: (
+    attributeName: string,
+    attributeValue: unknown
+  ) => NewRelicInteraction;
+};
+
+type NewRelicTraceData = {
+  start: number;
+  end: number;
+  name: string;
+  origin: string;
+  type: string;
+};
+
 declare global {
+  interface DocumentEventMap {
+    ["filezonedropaction"]: CustomEvent<{ files: FileList | null }>;
+  }
+
   interface IDBEventTarget extends EventTarget {
     result: IDBDatabase;
   }
 
   interface IDBEvent extends IDBVersionChangeEvent {
     target: IDBEventTarget;
+  }
+
+  interface DataTransfer {
+    dropEffect: "none" | "copy" | "link" | "move";
+    effectAllowed : "none" | "copy" | "copyLink" | "move";
+    setData: (format: string, data: string) => void
+  }
+
+  interface DragEvent extends Event { 
+    dataTransfer: DataTransfer
   }
 
   interface File extends Blob {
@@ -23,6 +77,8 @@ declare global {
 
   interface Navigator {
     readonly standalone?: boolean;
+    msSaveBlob?: (blob: Blob, defaultName?: string) => boolean;
+    msSaveOrOpenBlob: (blobOrBase64: Blob | string, filename: string) => void;
   }
 
   interface Window {
@@ -36,6 +92,35 @@ declare global {
       ): string & Stringified<T>
       parse<T>(text: string | Stringified<T>, reviver?: (key: any, value: any) => any): T | null
       parse(text: string, reviver?: (key: any, value: any) => any): any
-    }
+    };
+    Cookies: {
+      get: (name: string) => string | undefined
+    };
+    bindingSpan: Span | undefined;
+    startBindingSpan: (spanName: string, traceId: string, spanId: string, traceFlags: number) => void;
+    Headway: {
+      init: (config: HeadwayConfig) => void;
+    };
+    newrelic: {
+      noticeError: (error: Error, payload: Record<string, unknown>) => void;
+      finished: (time?: Date) => void;
+      addToTrace: (traceData: NewRelicTraceData) => void;
+      addRelease: (releaseName: string, releaseId: string) => void;
+      addPageAction: (
+        actionName: string,
+        JSONPayload: Record<
+          string,
+          string | boolean | unknown[] | null | number
+        >
+      ) => void;
+      setCurrentRoutename: (routeName: string) => void;
+      setCustomAttribute: (
+        attributeName: string,
+        attributeValue: number | string
+      ) => void;
+      setErrorHandler: (callback: (error: Error) => boolean) => void;
+      setPageViewName: (viewName: string, viewHost?: string) => void;
+      interaction: () => NewRelicInteraction;
+    };
   }
 }
