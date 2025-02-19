83617674a9ab00cb72be1e0d71e37b2a14b6df58
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-19 19:28:42 +0100

    Update string-utils.ts

--- src/helpers/string-utils.ts
+++ src/helpers/string-utils.ts
@@ -1,13 +1,28 @@
-/*
-*/
-export const removeHyphens = (str: string) => {
-  if (typeof str !== "string") {
-    throw new TypeError("removeHyphens: argument 1 is not a string");
+/**
+ * removeHyphens:
+ *
+ * @param {String} stringWithHyphens
+ *
+ * @returns {String}
+ *
+ */
+export const removeHyphens = (stringWithHyphens: string) => {
+  if (typeof stringWithHyphens !== "string") {
+    throw new TypeError("removeHyphens(...): argument 1 is not a string");
   }
 
-  if ('replaceAll' in str) {
-    return number.replaceAll("-", "");
+  if ('replaceAll' in stringWithHyphens) {
+    return stringWithHyphens.replaceAll("-", "");
   }
 
-  return str.replace(/-/g, "");
+  return stringWithHyphens.replace(/-/g, "");
 };
+
+/*!
+ * @EXAMPLE:
+ *
+ * const stringWithoutHyphens = removeHyphens("hello-world");
+ *
+ * console.log(stringWithoutHyphens) // "helloworld"
+ *
+ */
