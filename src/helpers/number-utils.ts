81353c7914c6093d6ea710c370847bf046b79f16
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-22 23:34:33 +0100

    Update number-utils.ts

--- src/helpers/number-utils.ts
+++ src/helpers/number-utils.ts
@@ -78,3 +78,38 @@ export const getShortSuffixForAmount = (amount: number): string => {
  *
  */
 
+/**
+ * getShortSuffixForBytes:
+ *
+ * @param {Number} bytes
+ * @param {Object=} options
+ *
+ * @returns {String}
+ */
+export function getShortSuffixForBytes(
+  bytes: number,
+  opts: {
+    decimals?: number
+    sizeType?: "accurate" | "normal"
+  } = {}
+) {
+  const { decimals = 0, sizeType = "normal" } = opts
+
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+  const accurateSizes = ["Bytes", "KiB", "MiB", "GiB", "TiB"]
+  if (bytes === 0) return "0 Byte"
+  const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+    sizeType === "accurate"
+      ? (accurateSizes[i] ?? "Bytes")
+      : (sizes[i] ?? "Bytes")
+  }`
+}
+
+/*!
+ * @EXAMPLE:
+ *
+ * const formatedBytes = getShortSuffixForBytes(1024, { sizeType: "accurate" });
+ *
+ * console.log(formatedBytes) // "1 KiB"
+ */
