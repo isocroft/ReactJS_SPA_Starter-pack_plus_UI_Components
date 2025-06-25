8470d9aab04d6653be942d7975bdc8b9bd4cdafd
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-25 23:04:38 +0100

    Create artifacts.tsx

---
+++ src/components/UI_Regions/VehicleParts/artifacts.tsx
@@ -0,0 +1,14 @@
+import type { UseQueryResult } from "@tanstack/react-query";
+
+export const useRegionDataLoader = () => {
+  /* @HINT: Pretending to be a call to `useQuery()` */
+  const query = {
+    data: [{ make: "Toyota", partId: 11 }, { make: "Nissan", partId: 6 }],
+    isLoading: false,
+    isError: false,
+    status: 'success',
+    refetch: () => Promise.resolve({});
+  };
+
+  return { vehicle: query } as Record<string, UseQueryResult>;
+};
