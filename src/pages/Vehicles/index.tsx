dfa0404181eb2d38605efc1a155ec89d23046098
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-25 23:32:46 +0100

    Update index.tsx

--- src/pages/Vehicles/index.tsx
+++ src/pages/Vehicles/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { VehicleParts } from "../../components/UI_Regions/VehicleParts/index";
+
 import type { UseQueryResult } from "@tanstack/react-query";
 
-const Vehicles = (injected: { queries: Record<string, UseQueryResult | null> } | undefined) => {
+const Vehicles = (injected: { queries: Record<("vehicles" | string & {}), UseQueryResult | null> } | undefined) => {
   if (!injected || !injected.queries) {
     return null;
   }
-  return <h1>{"Vehicles"}</h1>;
+  return (<section>
+    <h1>{"Vehicles"}</h1>
+    <VehicleParts queries={{ vehicleParts: [] }} />
+    </section>);
 };
 
 export default Vehicles;
