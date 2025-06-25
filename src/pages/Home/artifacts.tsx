f3320cf14cd9a170a41f9984d1534a5362fd79be
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-25 22:50:40 +0100

    Create artifacts.tsx

---
+++ src/pages/Home/artifacts.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import type { UseQueryResult } from "@tanstack/react-query";
+
+import type { StaticContext, RouteComponentProps } from "react-router";
+import type { Location } from "history";
+
+import { breadcrumbsMap } from "../../routes/routes.breadcrumbs.map";
+import { useRoutingBreadCrumbsData } from "../../layouts/GlobalRoutingProvider";
+import { RoutePaths } from "../../routes/routes.paths";
+
+export const usePageDataLoader = () => {
+  /* @HINT: Pretending to be a call to `useQuery()` */
+  const query = {
+    data: [{ id: 123 }, { id: 456 }],
+    isLoading: false,
+    isError: false,
+    status: 'success',
+    refetch: () => Promise.resolve({});
+  };
+
+  return { home: query } as Record<string, UseQueryResult>;
+};
+
+export const RoutePath = RoutePaths.HOME;
+
+export const PageHeader = ({
+  history
+}: Pick<RouteComponentProps<{}, StaticContext, object>, "history"> & {
+  queries: Record<string, UseQueryResult | null>,
+  user: {
+    permission: string;
+    bio?: Record<string, string | number>;
+  };
+}) => {
+  const BreadCrumbsList = useRoutingBreadCrumbsData(breadcrumbsMap, {
+    onNavigation ({ previousPathname }) {
+      console.log("navigation=>prev: ", previousPathname);
+    }
+  });
+  return (
+    <>
+      <h2>{"Home"}</h2>
+      <div id="breadcrumbs">
+       <BreadCrumbsList className="" />
+      </div>
+    </>
+  );
+};
+
+export const PageTitle = "Home *";
+
+export const renderPage = (
+  location: Location,
+  queries: Record<string, UseQueryResult | null>,
+  PageElement: React.LazyExoticComponent<
+    React.ComponentType<{ queries: Record<string, UseQueryResult | null> } | undefined>
+  >
+) => {
+  return <PageElement key={location.key} queries={queries} />;
+};
