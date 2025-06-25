fcf90123bb98f35de87f79815bf90ba62794c8dd
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-25 23:11:38 +0100

    Update index.tsx

--- src/components/UI_Regions/VehicleParts/index.tsx
+++ src/components/UI_Regions/VehicleParts/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import type { UseQueryResult } from "@tanstack/react-query";
 
-const VehicleParts = (injected: { queries: Record<string, UseQueryResult | null> } | undefined) => {
-  if (!injected || !injected.queries) {
+const VehicleParts = (injected: { queries: Record<("vehicleParts" | string & {}), UseQueryResult | null> } | undefined) => {
+  if (!injected || !injected.queries || !injected.queries.vehicleParts) {
     return null;
   }
 
   return (<>
     <h1>{"Vehicles Parts"}</h1>
-    <ul>{queries.data.map((datum) => {
+    <ul>{injected.queries.vehicleParts.data.map((datum) => {
       return (<li>{datum.partId} - {datum.make}</li>)
     })}</ul>
   </>);
