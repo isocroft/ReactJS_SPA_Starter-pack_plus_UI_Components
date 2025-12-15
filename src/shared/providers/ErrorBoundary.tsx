a7a5d90b731457d3ad008e496a3696d669ecc675
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2025-12-15 19:32:21 +0100

    refactor: reworked the `ErrorBoundary` component
    Refactor ErrorBoundary to improve error handling and type definitions.

--- src/shared/providers/ErrorBoundary.tsx
+++ src/shared/providers/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+// import { useSearchParamStateValueUpdate } from "react-busser";
 import { withRouter } from "react-router";
 
 // import Bugsnag from "@bugsnag/js"; // v7.13.2
@@ -25,11 +25,18 @@ if (config.PROD) {
 }
 */
 
-import type { ComponentType, ErrorInfo, ReactNode } from "react";
+import type { ComponentType, ErrorInfo, ReactNode, FunctionComponent } from "react";
 import type { RouteComponentProps } from "react-router";
 import type { Location } from "history";
 
+// interface CustomWithRouterProps {
+//   location: Location;
+//   changeParam: ReturnType<typeof useSearchParamStateValueUpdate>;
+// }
+
 type OnErrorCallback = (event: Event, cb: (err: null | Error, shouldSend?: boolean) => void) => void | boolean | Promise<void | boolean>;
+
+export type ErrorFallbackUIProps = { error: Error, location: Location, resetErrorBoundary?: (...args: any[]) => void };
 
 interface ErrorBoundaryProps extends RouteComponentProps {
   onError?: OnErrorCallback;
@@ -38,7 +45,7 @@ interface ErrorBoundaryProps extends RouteComponentProps {
     info: ErrorInfo
     clearError: () => void
   }>;
-  FallbackUI?: React.FunctionComponent<{ location: Location, error: Error }>;
+  FallbackUI?: FunctionComponent<ErrorFallbackUIProps>;
   children: ReactNode;
 }
 
@@ -46,6 +53,22 @@ interface ErrorBoundaryState {
   hasError: boolean;
   error: Error | null;
 }
+
+// @NOTE: React-Router v7.x.x doesn't support `withRouter(...)` since it's been deprecated and removed as from v6.x.x
+// @CHECK: https://stackoverflow.com/a/71097818
+// export function withRouter<ComponentProps>(Component: ComponentType<ComponentProps>) {
+//     function ComponentWithRouterProp(props: Omit<Readonly<ComponentProps & CustomWithRouterProps>, keyof CustomWithRouterProps>) {
+//         const location = useLocation();
+//         const changeSearchParam = useSearchParamStateValueUpdate("errorMessage");
+
+//         return <Component {...props as ComponentProps} location={location} changeParam={changeSearchParam} />;
+//     }
+
+//     return ComponentWithRouterProp;
+// }
+
+const DEV_ENV = process.env.;
+const PROD_ENV = import.meta.env.PROD;
 
 class ErrorBoundaryComponent extends React.Component<
   ErrorBoundaryProps,
@@ -63,17 +86,28 @@ class ErrorBoundaryComponent extends React.Component<
 
   componentDidCatch(error: Error, info: ErrorInfo) {
     // Example "componentStack":
-    //   in ComponentThatThrewAnError (created by App)
+    //   in ComponentThatThrows (created by App)
     //   in ErrorBoundary (created by App)
     //   in div (created by App)
     //   in App
-    
-    /* @TODO: Replace this with own error reporting logic (e.g. BugSnag). */
-    console.error(
-      "Uncaught error:",
-      error, // error.message, error.stack
-      info // info.componentStack
-    );
+    if (DEV_ENV) {
+        console.error(
+            "Uncaught error:",
+            error, // error.message, error.stack
+            info // info.componentStack
+        );
+    } else if (PROD_ENV) {
+        // @TODO: Replace this `console.info('.')` line with error reporting logic.
+        // @FIXME: Use [TrackJS], [Sentry] or [BugSnag] 
+        console.info('.');
+    }
+  }
+
+  resetErrorBoundary() {
+    this.setState({ hasError: false, error: null });
+    if (DEV_ENV) {
+        console.info("Error Boundary reset.");
+    }
   }
 
   componentDidUpdate (prevProps: ErrorBoundaryProps, prevState: ErrorBoundaryState) {
@@ -84,10 +118,10 @@ class ErrorBoundaryComponent extends React.Component<
       return;
     }
 
-    if (this.state.hasError) {
+    if (this.state.hasError && this.state.error !== null) {
       switch (this.state.error.message) {  
         case "Invalid Permissions":
-          this.props.history.replace('/?errorMessage=Invalid%20Permissions');
+          this.props.history.replace(`${window.location.pathname}?errorMessage=Invalid%20Permissions`);
         break;
       }
     }  
@@ -97,7 +131,7 @@ class ErrorBoundaryComponent extends React.Component<
     if (this.state.hasError && this.state.error !== null) {
       const { FallbackUI } = this.props;
       // Render any custom Fallback UI
-      return FallbackUI ? <FallbackUI error={this.state.error} location={this.props.location} /> : <p>Error: {this.state.error.message}</p>;
+      return FallbackUI ? <FallbackUI error={this.state.error} location={this.props.location} /> : <p>Error: {this.state.error?.message}</p>;
     }
 
     return this.props.children;
@@ -145,9 +179,13 @@ if (config.PROD) {
   Bugsnag.leaveBreadcrumb("[App.tsx]: App starting…");
 }
 
-export const ErrorBoundary = config.PROD
+const ErrorBoundary = config.PROD
   ? Bugsnag.getPlugin("react")?.createErrorBoundary(React) ?? withRouter(ErrorBoundaryComponent)
   : withRouter(ErrorBoundaryComponent);
+
+export default ErrorBoundary;
 */
 
-export const ErrorBoundary = withRouter(ErrorBoundaryComponent);
+const ErrorBoundary = withRouter(ErrorBoundaryComponent);
+
+export default ErrorBoundary;
